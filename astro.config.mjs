5d1bc0484890cafd54d9cbd53f5487180ff32b8d
Manuel Cascallar <cascallarmanuel@gmail.com>
2024-06-22 14:50:41 -0300

    fist pass

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from "astro/config";
+import glsl from "vite-plugin-glsl";
+
+// https://astro.build/config
+export default defineConfig({
+  server: {
+    host: true,
+  },
+  vite: {
+    plugins: [glsl()],
+    build: {
+      assetsInlineLimit: 0,
+      rollupOptions: {
+        output: {
+          assetFileNames: "[ext]/[name][extname]",
+          entryFileNames: "script/entry.js",
+        },
+      },
+      cssCodeSplit: false,
+    },
+  },
+});
